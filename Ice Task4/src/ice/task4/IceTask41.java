098c53ed408b9ea581d79ce29ede581c9f2be75d
Rataleenoch <ST10486877@rcconnect.edu.za>
2025-05-20 12:46:07 +0000

    Add files via upload

---
+++ Ice Task4/src/ice/task4/IceTask41.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ice.task4;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class IceTask41 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int total = 0;
+        for(int i=1;i<50;i++){
+            if (i%2==0){
+            total = i;
+            }
+        
+            System.out.println(total);
+        }
+    }
+    
+}
